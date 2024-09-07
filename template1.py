f70f719c7d74d362d789b23d1087bfbb25386181
sunilsala88 <sunilsala88@gmail.com>
2024-09-07 15:40:32 +0530

    hh

--- template1.py
+++ template1.py
@@ -1,20 +1,59 @@
 import random
 import time
 import datetime
+import pandas as pd
+
+#getting all active orders
+from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import GetOrdersRequest
+from alpaca.trading.enums import OrderSide, QueryOrderStatus
+import pandas as pd
+api_key='PKB872RM09BHVAMWPS9P'
+secret_key='yFTUvVwM5f9G0mrZJWFIZkZu5eF4mxbAq5b4wAFy'
+trading_client = TradingClient(api_key, secret_key, paper=True)
+
+
+def get_all_open_orders():
+    # params to filter orders by
+    request_params = GetOrdersRequest(
+                        status=QueryOrderStatus.OPEN
+                    )
+
+    # orders that satisfy params
+    orders = trading_client.get_orders(filter=request_params)
+    new_order=[]
+    for elem in orders:
+        new_order.append(dict(elem))
+
+    order_df=pd.DataFrame(new_order)
+    return order_df
+
+def get_all_position():
+
+    pos=trading_client.get_all_positions()
+
+
+    new_pos=[]
+    for elem in pos:
+        new_pos.append(dict(elem))
+
+    pos_df=pd.DataFrame(new_pos)
+    # pos_df.to_csv('pos.csv')
+    return pos_df
 
 
 def main_strategy_code():
-    t=random.randint(1,30)
-    print('executing the code it will take',t,'seconds')
-    time.sleep(t)
-
+    pos_df=get_all_position()
+    ord_df=get_all_open_orders()
+    print(pos_df)
+    print(ord_df)
 
 
 current_time=datetime.datetime.now()
 print(current_time)
 
 start_hour,start_min=15,16
-end_hour,end_min=15,20
+end_hour,end_min=15,45
 
 start_time=datetime.datetime(current_time.year,current_time.month,current_time.day,start_hour,start_min)
 end_time=datetime.datetime(current_time.year,current_time.month,current_time.day,end_hour,end_min)
